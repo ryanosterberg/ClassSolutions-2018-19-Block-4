80673d4a7618166b49e4c5fbdbc9c758085abba7
Jazlynn <21hansej@elmbrookstudents.org>
2018-11-29 14:08:35 -0600

    Split Fix

--- src/splitRandomSentence/splitRandomSentence.java
+++ src/splitRandomSentence/splitRandomSentence.java
@@ -7,14 +7,14 @@ public class splitRandomSentence {
 
 	public static void main(String[] args) {
 		Random generator = new Random();
-		int max = words.length;
-		int min = 0;
-		int randomWord;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Sentence:");
 		
 		String sentence = input.nextLine();
 		String[] words = sentence.split(" ");
+		int max = words.length;
+		int min = 0;
+		int randomWord;
 		randomWord = generator.nextInt(max - min + 1);
 		System.out.println(words[randomWord]);
 
